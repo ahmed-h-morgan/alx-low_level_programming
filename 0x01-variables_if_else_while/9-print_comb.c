b5b9ce63c89ad3530f312a41a2ea216f9ad2cffc
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-03-20 16:52:21 -0700

    final man task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x == 9)
+		{
+			putchar('0' + x);
+		}
+		else
+		{
+			putchar('0' + x);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
