f4ecb7a7c5dec9850a175987c101a560f46adbbc
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-03-20 18:22:26 -0700

    ;ll

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2;
+	
+	num1 = 0;
+	num2 = 0;
+	while (num1 <= 9)
+	{
+		while (num2 <= 9)
+		{
+			if (num1 != num2 && num1 < num2)
+			{
+				putchar(num1 + 48);
+				putchar(num2 + 48);
+				if (num1 + num2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}	num2++;
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
