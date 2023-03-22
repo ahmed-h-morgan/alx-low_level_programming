93d861c3a40574506def42a966de76655e1e4678
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-03-22 08:23:07 -0700

    10x

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -5,10 +5,10 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
+void print_alphabet_x10(void);
 
 int main(void)
 {
-    print_alphabet();
+    print_alphabet_x10();
     return (0);
 }
