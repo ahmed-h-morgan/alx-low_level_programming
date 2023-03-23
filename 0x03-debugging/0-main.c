cbf4e7e5700d34cd78791621b060a964cd55e39b
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-03-23 05:03:00 -0700

    pos or neg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,16 +9,7 @@ void positive_or_negative(int i)
 {
 	srand(time(0));
 	i = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i < 0)
-	{
-		printf("%d is negative\n", i);
-	}
-	else
+	if (i == 0)
 	{
 		printf("%d is zero\n", i);
 	}
