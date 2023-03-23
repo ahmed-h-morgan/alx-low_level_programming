6047d8c2fc17d126f04920ea3851bfa3185ef4e7
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-03-23 07:13:58 -0700

    largest number

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -3,7 +3,13 @@
 
 /**
 * main - prints the largest of 3 integers
-* Return: 0
+*
+* largest_number - returns the largest of 3 numbers
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*
 */
 
 int largest_number(int a, int b, int c);
