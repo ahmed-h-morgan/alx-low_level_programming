2f6f12c9e71246c9e90b585ffc19123f2da5079f
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-03-23 05:17:16 -0700

    pos or neg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * main - tests function that prints if integer is positive or negative
- * Function (positive_or_negative) : retun a feedback
- * Parameter (i) -  is a tested number
- * Return: 0
- */
-void positive_or_negative(int i)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
 {
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
