3dedff038955eb6287e6ace0c14651921ef44f82
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-03-23 03:43:57 -0700

    positve or negative debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Function (positive_or_negative) : retun a feedback
+ * Parameter (i) -  is a tested number
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}
