2576476d744cbf1ce12b0e42187f0db6e401c49a
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-03-22 19:33:11 -0700

    islower()

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -5,10 +5,18 @@
  *
  * Return: Always 0.
  */
-void print_alphabet_x10(void);
+int _islower(int c);
 
 int main(void)
 {
-    print_alphabet_x10();
-    return (0);
+	int r;
+	
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	 _putchar(r + '0');
+	_putchar('\n');
+ 	 return (0);
 }
