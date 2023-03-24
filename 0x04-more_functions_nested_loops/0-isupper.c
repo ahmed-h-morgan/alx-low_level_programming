b4f984f14e2d3e175c0d1aeb9182d5f264668591
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-03-24 14:37:23 -0700

    correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,17 @@
 #include "main.h"
-/*
- *
- *
-  */
+
+/**
+ * _isupper - returns 1 for the upper char
+ * @c: the character
+ * Return: largest number
+ *
+*/
 
 int _isupper(int c)
 {
-	for (c = 0; c <= 20; c++)
+	for (c = 65; c <= 90; c++)
 	{
-		return(1);
+		return (1);
 	}
 	return (0);
 }
-
