f6d4642593a04db665ab8d214c99c1877bbaace0
ahmedmorgan210 <ahmedmorgan210@github.com>
2023-04-04 03:50:06 +0200

    pre

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,27 +10,27 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-        unsigned int i;
+		unsigned int i;
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
+		i = 0;
+		while (i < size)
+		{
+				if (i % 10)
+				{
+						printf(" ");
+				}
+				if (!(i % 10) && i)
+				{
+						printf("\n");
+				}
+				printf("0x%02x", buffer[i]);
+				i++;
+		}
+		printf("\n");
 }
 char *_memset(char *s, char b, unsigned int n)
 {
-    char *s;
-    char b;
-    unsigned int n;
+	char *s;
+	char b;
+	unsigned int n;
 }
