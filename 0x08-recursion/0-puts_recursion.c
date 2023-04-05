e1afafb773fbe6e71a1ac562d1417438340dbb32
ahmedmorgan210 <ahmedmorgan210@github.com>
2023-04-05 14:55:06 +0200

    Recursion Day

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string char.
+ * Return: printed string
+*/
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (*(s + i) != '\0')
+	{
+		_putchar(*s + i);
+		i++;
+		_puts_recursion(char *s);
+	}
+	else
+	{
+		return;
+	}
+	_putchar('\n');
+}
