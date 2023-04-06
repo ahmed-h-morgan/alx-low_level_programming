ca5bfa131b8c4af37f0cf1ea982035c06965d135
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-06 02:46:28 +0200

    first task with mistake

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,4 +3,30 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: the string
- *
+ * Return: printed string
+*/
+
+void _print_rev_recursion(char *s)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	if (*(s + i) <= '\0' && j == 0)
+	{
+		if (*s == '\0')
+		{
+			j = 1;
+		}
+		else if (*s != '\0')
+		{
+			i++;
+		}
+	}
+	if (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+		_print_rev_recursion(s + i);
+	}
+}
