749d1c404ccee9f39781c74aa4ae859d10bd7d3f
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-07 03:03:02 +0200

    reversed string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,37 +5,13 @@
  * @s: the string
  * Return: printed string
 */
-void string_end(int i, int j, char *s);
+
 void _print_rev_recursion(char *s)
 {
-	int i, j;
-	/*char c;*/
+	int i;
 
 	i = 0;
-	j = 0;
-	if (j == 0)
-	{
-		string_end(i, j, s);
-	}
-	else if (i >= 0 && j > 0)
-	{
-		_putchar(*s + i);
-		i--;
-		_print_rev_recursion(s + i);
-	}
-}
-void string_end(int i, int j, char *s)
-{
-	/*char *s;*/
-	if (*s != '\0')
-	{
-		*s = *(s + i);
-		i++;
-		string_end(i, j, s);
-	}
-	else
-	{
-		j += 1;
-	}
-	/*return (j)*/
+	i++;
+	_print_rev_recursion(s + i);
+	_putchar(*s);
 }
