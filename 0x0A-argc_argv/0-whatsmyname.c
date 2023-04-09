6109b540a5b9ed23049167d4995ed10a06297da8
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-09 13:58:33 +0200

    program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,14 @@
  * Return: (0) is successful
 */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+/*int main(__attribute__((unused)) int argc, char *argv[])*/
+int main(int argc, char *argv[])
 {
+	if (argc > 1)
+	{
+		printf("wrong input");
+		return (1);
+	}
 	printf("%s \n", argv[0]);
 	return (0);
 }
