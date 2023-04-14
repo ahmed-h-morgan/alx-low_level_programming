8edab2e32484e234caec3abe3f165f673ea1b67a
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-14 05:23:57 -0700

    alpah 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,20 @@
 #include "main.h"
-/*    */
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * Return: 0 if successful
+*/
 void print_alphabet_x10(void)
 {
-	int x, y;
+	int i, j;
 
-	for (y = 0; y < 10; y++)
+	i = 0;
+	while (i <= 10)
 	{
-		for (x = 'a'; x <= 'z'; x++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar(x);
+			_putchar(j);
 		}
+		i++;
 		_putchar('\n');
 	}
 }
