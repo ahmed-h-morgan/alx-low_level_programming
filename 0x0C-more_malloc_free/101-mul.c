55b248e507a6a6c5c8cc6661f3e8ebf9c28c88cb
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-13 19:54:30 -0700

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,73 +4,101 @@
 #include <ctype.h>
 #include "main.h"
 
-/**
- * print_error_and_exit - check error
- *
-*/
-int _putchar(char c);
+#define ERR_MSG "Error"
 
-void print_error_and_exit(void)
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
 {
 	printf("Error\n");
 	exit(98);
 }
+
 /**
- * check_valid_number - check valid num
- * @num: the number
- * Return: 0 in error
-*/
-
-void check_valid_number(char *num)
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
 {
-	int i;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	for (i = 2; num[i] != '\0'; i++)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		if (!isdigit(num[i]))
-		{
-			print_error_and_exit();
-		}
-	}
-}
-/**
- * multiply - multiply numbers
- * @num1: the first nuber
- * @num2: the second number
- * Return: multiplied num
-*/
-void multiply(char *num1, char *num2)
-{
-	int len1 = strlen(num1), len2 = strlen(num2);
-	int *result = NULL;
-	int i, j, carry, sum;
-
-	result = calloc(len1 + len2, sizeof(int));
-	if (result == NULL)
-	{
-		printf("Error: Memory allocation failed\n");
-		exit(1);
-	}
-	for (i = len1 - 1; i >= 0; i--)
-	{
+		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			sum = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
-			result[i + j + 1] = sum % 10;
-			carry = sum / 10;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		result[i + j + 1] = carry;
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	i = 0;
-	while (i < len1 + len2 - 1 && result[i] == 0)
+	for (i = 0; i < len - 1; i++)
 	{
-		i++;
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	for (; i < len1 + len2; i++)
-	{
-		printf("%d", result[i]);
-	}
-	printf("\n");
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
 	free(result);
+	return (0);
 }
+
