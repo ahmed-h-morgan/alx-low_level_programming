4f3aff5b59d84aa519736b93b5c5a34d055fef40
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-14 05:37:37 -0700

    alpah 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,12 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	i = 0;
-	while (i <= 10)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
 		_putchar('\n');
-		i++;
-		/*_putchar('\n');*/
 	}
-	/*_putchar('\n');*/
 }
