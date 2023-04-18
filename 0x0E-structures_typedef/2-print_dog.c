6e1e1514ade377e56ee59b28c285f7520d7e1d86
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-17 19:14:57 -0700

    new dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints dog
+ * @d: the struct
+ * Return: 0 successful
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
