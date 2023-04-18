b29d33c740edc8ba781827be7b0412aa618c7091
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-17 18:13:48 -0700

    the init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog
+ * @dog *d: the struct
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ * @d: the struct
+ * Return - 0 suceesful
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,9 @@ struct dog
 
 /* Declare any functions you want to use with struct dog here */
 
-#endif /* DOGS_H */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
