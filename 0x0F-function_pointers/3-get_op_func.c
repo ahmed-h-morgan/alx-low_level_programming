4fc7203f03f9f8b472d07306d480b57a4633106d
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-19 20:11:12 -0700

    100

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,19 +3,36 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 
+/**
+ * get_op_func - Selects the correct function to perform the operation
+ * @s: The operator passed as argument.
+ * Return: A pointer to the operator given as a parameter.
+ */
 
-int (*get_op_func(char *s))(int, int) {
-    if (*s == '+') {
-        return &op_add;
-    } else if (*s == '-') {
-        return &op_sub;
-    } else if (*s == '*') {
-        return &op_mul;
-    } else if (*s == '/') {
-        return &op_div;
-    } else if (*s == '%') {
-        return &op_mod;
-    } else {
-        return NULL;
-    }
+int (*get_op_func(char *s))(int, int)
+{
+	if (*s == '+')
+	{
+		return (&op_add);
+	}
+	else if (*s == '-')
+	{
+		return (&op_sub);
+	}
+	else if (*s == '*')
+	{
+		return (&op_mul);
+	}
+	else if (*s == '/')
+	{
+		return (&op_div);
+	}
+	else if (*s == '%')
+	{
+		return (&op_mod);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
