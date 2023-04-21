e60e251ce1b8431cb2bac0e5a564ce3a9f148349
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-20 19:28:03 -0700

    variadic

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(strings, n);
 
-	for (i = 0; i < n; index++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(strings, char *);
 
