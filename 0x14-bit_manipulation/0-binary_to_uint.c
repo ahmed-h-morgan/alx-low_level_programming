ae002c4bf8650b20e88d15e243559030e32ed6db
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-08-07 20:36:25 +0200

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,10 +6,20 @@
  * (there is one or more chars in the string b that is not 0 or 1)
  * (b is NULL)
 */
+
+unsigned int pow(*base, *index)
+{
+	/* this func is to calc the power manually*/
+	int n, i;
+	for (n = 1; n < index; n++)
+	{
+		
+	}
+}
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int len, index, num;
-
+	unsigned int base = 2;
 	len = 0;
 	num = 0;
 	if (b == NULL)
@@ -25,7 +35,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[len - 1] == '1')
 		{
-			num += '1' << index;
+			num +=  * (2 ** index);
+			/*num += '1' << index;*/
 		}
 		len--;
 	}
