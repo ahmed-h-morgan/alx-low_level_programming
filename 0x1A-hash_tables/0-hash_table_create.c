4d71d91ca81e07bb72a4d8a824783b39b16725e4
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-12-03 22:02:57 -0800

    update the 0-task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,30 @@
 #include "hash_tables.h"
 
 
-
+/*
+ * Create node function in seperate function
+ * link every index of the array of table size with a node function
+ *
+ * 
+ * */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_node_t *item = malloc(sizeof(hash_node_t));
+	if(!item)
+	{
+		return(NULL);
+	}
+	item->key = strdup(key);
+	item->value = strdup(value);
+	item->next = NULL;
+
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	if (!table)
+	{
+		return (NULL);
+	}
+	unsigned long int table->size = size;
 	
+
 
 }
