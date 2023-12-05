3b873b72e462dc4fbec59c21cdbcfda099b2417e
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-12-04 19:16:58 -0800

    try to create the hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -32,7 +32,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	table->size = size;
-	table->array = (hash_node_t **item) calloc(table->size, sizeof(hash_node_t));
+	table->array = (hash_node_t**) calloc(table->size, sizeof(hash_node_t));
 
 	for(i = 0; i < size; i++)
 	{
