f319d300a5e4f344d9f6e9cab3344304a1f59b3c
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-12-05 19:20:51 -0800

    try to fix the issue of amount of memory allocated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,20 @@
 
 hash_node_t *create_item(char *key, char *value)
 {
-	hash_node_t *item = malloc(sizeof(hash_node_t));
+	hash_node_t *item = (hash_node_t *)malloc(sizeof(hash_node_t));
 
 	if (!item)
 	{
 		return (NULL);
 	}
-	item->key = strcpy(item->key, key);
+	item->key = strdup(key);
 	item->value = strcpy(item->value, value);
 	item->next = NULL;
+
+
+	/*printf("Memory used by hash_node_t structure: %lu bytes\n", sizeof(hash_node_t));*/
+	/*printf("Memory used by key string: %lu bytes\n", strlen(item->key) + 1);*/
+	/*printf("Memory used by value string: %lu bytes\n", strlen(item->value) + 1);*/
 
 	return (item);
 }
@@ -31,8 +36,9 @@ hash_node_t *create_item(char *key, char *value)
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
+	printf("Memory used by hash_table_t structure: %lu bytes\n", sizeof(hash_table_t));
 	if (!table)
 	{
 		return (NULL);
@@ -45,12 +51,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		table->array[i] = NULL;
 	}
 
+	/*(*create_item)(char *, char *)=create_item;*/
 
-	printf("Memory used by hash_node_t structure: %lu bytes\n", sizeof(hash_node_t));
-	printf("Memory used by hash_table_t structure: %lu bytes\n", sizeof(hash_table_t));
-	printf("Memory used by key string: %lu bytes\n", strlen(key) + 1);
-	printf("Memory used by value string: %lu bytes\n", strlen(value) + 1);
-	printf("Memory used by array of hash_node_t pointers: %lu bytes\n", size_t * sizeof(hash_node_t *));
+	/*printf("Memory used by array of hash_node_t pointers: %lu bytes\n", size * sizeof(hash_node_t *));*/
 
 	return (table);
 
