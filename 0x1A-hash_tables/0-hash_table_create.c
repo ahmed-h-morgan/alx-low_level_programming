205cac669ae9efc0c45728e05dc2f57a56c4f834
root <root@Ahmed-M>
2023-12-08 17:11:43 +0200

    try

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(size * sizeof(hash_node_t));
+	hash_table_t *table = (hash_node_t **) malloc(size * sizeof(hash_node_t));
 	/*table->array = calloc(size, sizeof(hash_node_t));*/
 	return (table);
 }
