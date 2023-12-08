d48fd37606a9e32ff7697d216ea77216b77b9da3
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-12-08 08:23:12 -0800

    fix betty in creating hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,13 +1,34 @@
 #include "hash_tables.h"
 
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+*/
+
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 
-	hash_table_t *table = malloc(size * sizeof(hash_node_t));
-	if (!table)
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 	{
 		return (NULL);
 	}
-	
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	for(i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+
 	return (table);
 }
