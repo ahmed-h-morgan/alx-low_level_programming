fe2b4bf51495e7876262d0a383cc51cd0b1c7e66
root <root@Ahmed-M>
2023-12-08 17:31:51 +0200

    ertry

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,12 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+
 	hash_table_t *table = malloc(size * sizeof(hash_node_t));
-	/*table->array = calloc(size, sizeof(hash_node_t));*/
+	if (!table)
+	{
+		return (NULL);
+	}
+	
 	return (table);
 }
